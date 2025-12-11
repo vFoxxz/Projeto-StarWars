e1dcadcdd18decb086df30751f4724ab0dc966b7
Victor Fonseca <victor.fonseca0@gmail.com>
2025-12-10 23:49:06 -0300

    feito landing

--- js/ListenerDark.js
+++ js/ListenerDark.js
@@ -1,7 +1,13 @@
 window.addEventListener("message", (event) => {
+    const frase = document.querySelector(".frase-final");
+    const SoleLua = document.querySelector(".SoleLua");
     if (event.data === "enable-dark-mode") {
       document.body.classList.add("dark-mode");
+      frase.textContent = "Que a escuridão esteja com você";
+      SoleLua.src = "../src/img/lua.png";
     } else if (event.data === "disable-dark-mode") {
       document.body.classList.remove("dark-mode");
+      frase.textContent = "Que a força esteja com você";
+      SoleLua.src = "../src/img/sol.png";
     }
   });
--- js/darkMode.js
+++ js/darkMode.js
@@ -2,26 +2,21 @@ const toggle = document.getElementById("darkModeToggle");
 const logo = document.querySelector(".logo-star");
 const saber = document.querySelector(".logo-dark");
 
-
 window.addEventListener("DOMContentLoaded", () => {
     const prefsalva = JSON.parse(sessionStorage.getItem("preference"));
-    if (prefsalva.theme === "dark") {
+    if (prefsalva && prefsalva.theme === "dark") {
         document.body.classList.add("dark-mode");
-
         logo.src = "../src/img/Logo Branca.png";
         saber.src = "../src/img/sabre_vermelho.png";
         saber.classList.add("rotacionado");
-
         window.parent.postMessage("enable-dark-mode", "*");
     } else {
         logo.src = "../src/img/Logo Preta.png";
         saber.src = "../src/img/sabre_azul.png";
         saber.classList.remove("rotacionado");
-
         window.parent.postMessage("disable-dark-mode", "*");
     }
 });
-
 
 toggle.addEventListener("click", () => {
     const isDark = document.body.classList.toggle("dark-mode");
